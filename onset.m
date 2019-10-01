466545d356f125f9ab008c0f0537c0d15b018b31
jean_paul_ <jean_paul_@users.noreply.github.com>
2019-10-01 16:42:05 +0200

    Update onset.m

--- onset.m
+++ onset.m
@@ -6,6 +6,7 @@ file = 'drumloop120_mono.wav';
 tWindow = 0.030;
 nWindow = floor(tWindow*Fs);
 window = hann(nWindow,'periodic');
+% window = ones(nWindow,1);
 
 tHop = 0.010;
 tOverlap = tWindow-tHop;
@@ -23,7 +24,6 @@ colormap winter
 % Plots PSD using imagesc
 [S,f,t,P] = spectrogram(song,window,nOverlap,[],Fs,'yaxis');
 figure(2)
-caxis([-150 -50])
 imagesc(t,f*1e-3,10*log10(abs(P)))
 set(gca,'YDir','normal')
 colorbar
@@ -31,3 +31,48 @@ ylim([0 5])
 colormap winter
 
 % Plots STFT using logarithmic compression
+G = log(1+gamma*abs(S));
+figure(3)
+imagesc(t,f*1e-3,G)
+set(gca,'YDir','normal')
+colorbar
+ylim([0 5])
+colormap winter
+
+%% Compute discrete temporal derivative
+M = 26-4;
+M = 2*M+1;
+onsetThr = 0.13;
+
+flux = diff(G,1,2);
+flux = (flux + abs(flux))/2;
+% Plot novelty function without summing
+figure(4)
+imagesc(t,f*1e-3,flux)
+set(gca,'YDir','normal')
+colorbar
+ylim([0 5])
+colormap winter
+
+
+flux = sum(flux);
+mu = movmean(flux,M);
+% Plot novelty function and moving average
+figure(5)
+plot(t(1:end-1),flux)
+hold on
+plot(t(1:end-1),mu)
+hold off
+
+fluxEnhanced = flux - mu;
+fluxEnhanced = (fluxEnhanced+abs(fluxEnhanced))/2;
+fluxEnhanced = fluxEnhanced/max(fluxEnhanced);
+
+onsets = fluxEnhanced > onsetThr;
+% Plot Enhanced and normalized spectral flux and plot onsets
+figure(6)
+plot(t(1:end-1),fluxEnhanced)
+hold on
+stem(t(1:end-1),onsets)
+yline(onsetThr)
+hold off
